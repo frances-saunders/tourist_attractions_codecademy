91c9279150bfd8b0a98a8d6981286b2d21709940
Frances Saunders <92679675+frances-saunders@users.noreply.github.com>
2022-09-11 20:13:15 -0400

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,3 +1,5 @@
+##This project fulfills the reqs for the "Tourist Attraction" project in Codecademy
+
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField, RadioField
 from wtforms.validators import DataRequired
